3e14cdf873e0596db19b74cd8a0eb72d37869f33
rulssss <marianopicco83@gmail.com>
2025-09-20 17:36:40 -0300

    se borro nueva forma recomendada de acl

--- infra/main.tf
+++ infra/main.tf
@@ -132,11 +132,6 @@ resource "aws_s3_bucket" "bucket" {
   bucket = "rulssss-pryect-tf-${random_string.bucket_suffix.result}"
 }
 
-# ACL separado (nueva forma recomendada)
-resource "aws_s3_bucket_acl" "bucket_acl" {
-  bucket = aws_s3_bucket.bucket.id
-  acl    = "private"
-}
 
 # Outputs 
 output "db_host" {
